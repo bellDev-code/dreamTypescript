c7dc3c9727ae33133e4fdb27d63ce2b3050872c5
bellDev-code <sumaoo20@naver.com>
2023-06-19 15:14:00 +0900

    javascript module & tsconfig

---
+++ 10-javascript/10-3-module1.js
@@ -0,0 +1,9 @@
+export default function add(a, b) {
+    return a + b;
+}
+
+export const number = 10;
+
+export function print() {
+    console.log('print')
+}
---
+++ 10-javascript/10-3-module2.js
@@ -0,0 +1,6 @@
+import add, * as calculator from "./10-3-module1.js"
+
+console.log(add(1, 2))
+
+calculator.print()
+calculator.number;
