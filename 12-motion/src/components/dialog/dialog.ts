2f8e948e8ba8d91272b56fe6f77c495e26288d4f
bellDev-code <sumaoo20@naver.com>
2023-07-06 16:27:26 +0900

    Code refactoring

--- 12-motion/src/components/dialog/dialog.ts
+++ 12-motion/src/components/dialog/dialog.ts
@@ -2,8 +2,17 @@ import { BaseComponent, Component } from "../component.js";
 import { Composable } from "../page/page.js";
 
 type OnCloseListener = () => void;
-
 type OnSubmitListener = () => void;
+
+export interface MediaData {
+  readonly title: string;
+  readonly url: string; 
+}
+
+export interface TextData {
+  readonly title: string;
+  readonly body: string;
+}
 
 export class InputDialog extends BaseComponent<HTMLElement> implements Composable {
     closeListener?: OnCloseListener;
